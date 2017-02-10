75f031f26d472021e2a0dc4249c90493bfec3cea
GitInMaRepo <oliver.s.roth@gmail.com>
2017-02-10 22:12:13 +0100

    initial commit

---
+++ TDD von Kopf bis Fuss/Zuerst_mal_das.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace TDD_von_Kopf_bis_Fuss
+{
+    [TestClass]
+    public class Zuerst_mal_das
+    {
+        [TestMethod]
+        public void DieSolutionSollBauenUndDerTestLaufen()
+        {
+            var fail = true;
+            fail.Should().BeTrue();
+        }
+    }
+}
