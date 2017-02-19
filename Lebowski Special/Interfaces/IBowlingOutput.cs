7041edb07f1ba4a59fecb68008c69f652d9699b4
GitInMaRepo <oliver.s.roth@gmail.com>
2017-02-19 23:11:59 +0100

    Set folders as final step

---
+++ Lebowski Special/Interfaces/IBowlingOutput.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Lebowski_Special
+{
+    public interface IBowlingOutput
+    {
+        string FormatOutput(List<int> result);
+    }
+}
---
+++ Lebowski Special/Interfaces/IBowlingService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Lebowski_Special
+{
+    public interface IBowlingService
+    {
+        void Add(Frame frame);
+        void CalculateGame();
+        Frame CreateFrame(string wurf1, string wurf2);
+        List<byte> GetLatestResult();
+    }
+}
